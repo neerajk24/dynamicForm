fa8f073e71c8456bf0c8f4ec0d84baf90ed9a7a9
Neeraj Kumar <kumarne@transitioncomputing.com>
2024-07-05 21:18:21 +0100

    test

--- src/app/form.service.ts
+++ src/app/form.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root'  // Ensures service is provided at the root level
 })
 export class FormService {
   constructor(private http: HttpClient) {}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideHttpClient(withFetch()),
-    importProvidersFrom(BrowserAnimationsModule)
+    provideHttpClient(withFetch()), // Ensuring fetch is used for HTTP calls
+    BrowserAnimationsModule
   ]
 }).catch(err => console.error(err));
