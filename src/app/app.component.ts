0e9582b51c9f4bf8db0d9912e3a7f23581790c20
Neeraj Kumar <kumarne@transitioncomputing.com>
2024-07-05 20:54:29 +0100

    Base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,22 @@
 import { Component } from '@angular/core';
 import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
-import { provideHttpClient } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    BrowserAnimationsModule,
+    CommonModule,
     ReactiveFormsModule,
     MatInputModule,
     MatFormFieldModule,
     MatButtonModule,
     DynamicFormComponent
   ],
-  template: '<app-dynamic-form></app-dynamic-form>',
-  providers: [provideHttpClient()]
+  template: '<app-dynamic-form></app-dynamic-form>'
 })
 export class AppComponent {}
--- src/app/dynamic-form/dynamic-form.component.ts
+++ src/app/dynamic-form/dynamic-form.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormService } from '../form.service';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
-import { FormService } from '../form.service';
-import { FormField } from './form-field.interface'; // Importing the FormField interface
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dynamic-form',
@@ -21,19 +19,18 @@ import { FormField } from './form-field.interface'; // Importing the FormField i
     MatButtonModule
   ],
   templateUrl: './dynamic-form.component.html',
-  styleUrls: ['./dynamic-form.component.css']
+  styleUrls: ['./dynamic-form.component.css'],
+  providers: [FormService]
 })
 export class DynamicFormComponent implements OnInit {
-  form: FormGroup = this.fb.group({});
-  formSchema: FormField[] = [];
+  form!: FormGroup;
+  formSchema: any[] = [];
 
   constructor(private fb: FormBuilder, private formService: FormService) {}
 
   ngOnInit(): void {
-    // Replace with your API endpoint
     const apiUrl = 'https://example.com/api/form-schema';
-    
-    this.formService.getFormSchema(apiUrl).subscribe((schema: FormField[]) => {
+    this.formService.getFormSchema(apiUrl).subscribe(schema => {
       this.formSchema = schema;
       this.createForm();
     });
